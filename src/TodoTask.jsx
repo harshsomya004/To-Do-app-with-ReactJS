6243010b751a888fc9cfd6ff4e44f1066ddd5191
Harsh Somya <183048@nith.ac.in>
2022-06-23 23:23:32 +0530

    Update Changes in Project

--- src/TodoTask.jsx
+++ src/TodoTask.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import {css} from "@emotion/css";
 
-function TodoTask(props)
-{
-  return(
-    <div>
-      {props.tasks.map((task,index)=>{
-        return <p onClick={()=> props.removeTask(task)} key={index}>{task}</p>
-      })}
-    </div>
+function TodoTask(props) {
+  return (
+      <div>
+        {props.tasks.map((task, index) => {
+          return (<div style={{
+            display: "flex",
+            flexDirection: "row", justifyContent: "space-between", alignContent: "center", padding: 10,
+            marginRight: 20
+          }}><p aria-multiline={"true"} key={index}>
+            {task}
+          </p>
+            <button className={css`
+                      height: 40px;
+                      background-color: #FFCCCB;
+                      border-radius: 12px`} onClick={() => props.removeTask(task)}>Remove
+            </button>
+          </div>)
+        })}
+      </div>
   )
 }
 
